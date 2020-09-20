8fa8c285d5bf989c2632a90b91e35bc6cad986bb
techitesh <techitesh@techiteshs-MacBook-Air.local>
2020-09-20 19:32:04 +0530

    Updated code

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    return res.status(200).json({ success: true, message: "triggered from webhook" })
+    return res.status(200).json({ success: true, message: "triggered from webhook!!" })
 })
 
 app.listen(3000, () => {
