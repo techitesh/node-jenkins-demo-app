ce21e3e189fd38e9bde495497b7dda73752dfe91
techitesh <techitesh@techiteshs-MacBook-Air.local>
2020-09-20 15:43:23 +0530

    Added capistrano

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     return res.status(200).json({ success: true })
